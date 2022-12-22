0c9750cf47ab9bd0b07ff03152923a9563d841d0
LitvinT <Litvin.lara74mail.ru@gmail.com>
2022-12-22 23:02:28 +0300

    Initial commit

---
+++ coffee/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import IndexTemplateView, ProductListView, WorkScheduleListView, AboutTemplateView
+
+
+urlpatterns = [
+    path('', IndexTemplateView.as_view(), name='index'),
+    path('about/', AboutTemplateView.as_view(), name='about'),
+    path('products/', ProductListView.as_view(), name='products'),
+    path('store/', WorkScheduleListView.as_view(), name='store')
+]
